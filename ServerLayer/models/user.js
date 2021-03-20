70f5b5975fcc79b6b3393a54e0a4a0b3185505e4
Vladimir <vladimir.golovkin16@yandex.ru>
2021-03-20 14:45:15 +0500

    add: new models

---
+++ ServerLayer/models/user.js
@@ -0,0 +1,42 @@
+
+module.exports = mongoose => {
+    const Schema = mongoose.Schema;
+
+    const userSchema = new Schema({
+        login: String,
+        password: String,
+        firstName: String,
+        lastName: String,
+        createdAt: Date,
+        avatar: String,
+        money: String,
+        orders: [{
+            type: Schema.Types.ObjectId,
+            ref: "Order"
+        }],
+        rewards: [{
+            type: Schema.Types.ObjectId,
+            ref: "Reward"
+        }],
+        tasks: [{
+            type: Schema.Types.ObjectId,
+            ref: "Task"
+        }],
+        job: {
+            type: Schema.Types.ObjectId,
+            ref: "Job"
+        },
+        shopStatus: {
+            type: Schema.Types.ObjectId,
+            ref: "ShopStatus"
+        }
+    }, { timestamps: true })
+
+    userSchema.method("toJSON", function() {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+    return mongoose.model("User", userSchema);
+};
